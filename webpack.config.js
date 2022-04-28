0118cd7ffd6566924d39eba062b42ca05a6c5ea6
jvymvn <713Ellington@gmail.com>
2022-04-27 20:25:59 -0500

    Bundle analyzer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
     mode: 'development',
     entry: {
@@ -54,6 +54,7 @@ module.exports = {
             title: 'Webpack App',
             filename: 'index.html',
             template: 'src/template.html'
-        })
+        }),
+        new BundleAnalyzerPlugin(),
     ],
 }
